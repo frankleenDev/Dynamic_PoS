c7c1bb5487be579597cdcf58b360706f1f45c39f
frankleenDev <frankgilbert911@gmail.com>
2019-05-23 17:35:10 +0300

    invoice updates

--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -31,6 +31,132 @@
 
         </div>
 
+        <!-- Invoice details -->
+        <div class="container-fluid row">
+          <!-- Order Section -->
+              <div class="col-lg-4">
+                <table class="table borderless">
+                  <tr>
+                    <td>
+                     <b>Order No: #</b> 
+                    </td>
+                    <td class="text-left">
+                     342
+                    </td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b>Issued By:</b> 
+                    </td>
+                    <td>
+                      Frank
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b> Time:</b> 
+                    </td>
+                    <td>
+                      16:05:22
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b>Date:</b> 
+                    </td>
+                    <td>
+                      02/06/2019
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <!-- End of order -->
+
+              <!-- Bill Section -->
+              <div class="col-lg-4">
+                <table class="table borderless">
+                  <tr>
+                    <td>
+                     <b>Bill To: </b> 
+                    </td>
+                    <td class="text-left">
+                     Customer
+                    </td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                   <tr>
+                    <td>
+                     <b> Company:</b> 
+                    </td>
+                    <td>
+                      ... Ltd
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b>Transportation:</b> 
+                    </td>
+                    <td>
+                      (Yes)Destination
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b>Installation:</b> 
+                    </td>
+                    <td>
+                      Inclusive
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <!-- End of bill -->
+
+              <!-- Bill Section -->
+              <div class="col-lg-4">
+                <table class="table borderless">
+                  <tr>
+                    <td>
+                     <b>Invoice No: </b> 
+                    </td>
+                    <td class="text-left">
+                     #065
+                    </td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                   <tr>
+                    <td>
+                     <b> Due date:</b> 
+                    </td>
+                    <td>
+                      ... Ltd
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b>Payment:</b> 
+                    </td>
+                    <td>
+                      (Yes)Destination
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                     <b>Installation:</b> 
+                    </td>
+                    <td>
+                      Inclusive
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <!-- End of bill -->
+        </div>
+
       </div>
 
 
